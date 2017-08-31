2084c2badb3bfc992aca19e47d9871b9ecc9742c
Nick ten Veen <nveen0930@gmail.com>
2017-08-31 11:41:47 +0200

    Lifted components can now have actions

--- icedust/lib-js/src/runtime/pixiedust/components/Lifted.js
+++ icedust/lib-js/src/runtime/pixiedust/components/Lifted.js
@@ -3,22 +3,44 @@ var PixieDustComponent = require('./PixieDustComponent');
 var Functions = require('lib/functions'); 
 
 
-function Lifted(render){
+function Lifted(render, actions){
+	if(actions === undefined){ actions = []; }
 	
 	function constructor(props, context){
+		this.actions = {};
+		for(var i = 0 ; i < actions.length ; i++){
+			var actionName = actions[i];
+			var actionCreator = function(){
+				var args = Array.prototype.slice.call(arguments);
+				var dispatch = this.dispatch.bind(this);
+				var action = function(e){
+					var message = {
+							type: actionName,
+							props: this.props,
+							args: args
+						};
+					console.log('message', message)
+					dispatch(message);
+				}
+				action = action.bind(this);
+				return action;
+			}
+			actionCreator = actionCreator.bind(this);
+			this.actions[actionName] = actionCreator; 
+		}
 		this.materialize(props, context);
 	}
 	
 	var LiftedComponent = Class(PixieDustComponent, {
 		constructor: constructor,
-		
-    componentWillReceiveProps: function(props, context){
+		componentWillReceiveProps: function(props, context){
       this.materialize(props, context);
     },
 
     materialize: function(props, context){
-      var state = context.store.getState();
-      var rendered = render(props, state);
+    	var state = context.store.getState();
+    	var scope = _.assign({}, props, this.actions);
+    	var rendered = render(scope, state);
       this.materialized = rendered.result;
       if(rendered.state !== state){
         this.stateUpdate(context, rendered.state);
